1d0c6a2e178baad1c89ecdd9149efdb37e288e6a
Sergio Aciar <aciarenator@gmail.com>
2023-04-18 15:11:55 -0300

    cree el comp. VisorCaracteristicasVisorStats

---
+++ src/components/VisorCaracteristicas.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps(['peso', 'altura','tipos']);
+</script>
+
+<template>
+    <table class="table table-striped ">
+        <tbody>
+            <tr>
+                <td>
+                    Weight
+                </td>
+                <td>
+                    {{ peso / 10 }} Kgs.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Height
+                </td>
+                <td>
+                    {{ altura/ 10 }} Mts.
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div v-for="tipo in tipos">
+        <div v-if="tipo.type.name === 'grass'" class="text-bg-primary p-3"> GRASS</div>
+
+    </div>
+</template>
+
+<style>
+    .grass{
+        background-color: #09ff00;
+    }
+</style>
