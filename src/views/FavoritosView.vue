91590847c1c8bbe3ffb7a1b30bf518625f45eed4
Sergio Aciar <aciarenator@gmail.com>
2023-04-23 21:40:24 -0300

    cambio de portada y disposicion en favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -16,8 +16,10 @@
                 <h1 class="display-3">Favoritos</h1>
                 <ul class="list-group">
                     <p v-if="favoritos.length == 0">No hay favoritos</p>
-                    <li v-else class="list-group-item d-flex justify-content-between" v-for="poke in favoritos" :key="poke.id">
-                        <span><img :src="poke.sprites?.front_default" alt=""></span>
+                    <li v-else class="list-group-item  justify-content-between" v-for="poke in favoritos" :key="poke.id">
+                        <span>
+                            <img :src="poke.sprites?.front_default" alt="">
+                        </span>
                         <h5 class="align-self-center">{{ poke.name }}</h5>
                         <span class="align-self-center">
                             <router-link class="btn btn-sm btn-primary me-2" :to="`/pokemons/${poke.name}`">Mas
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,35 +3,88 @@
 </script>
 
 <template>
-<div id="building">
+<!-- <div id="building">
     <p class="display-1 mt-4">Bienvenidos</p>
+</div> -->
+<div class="container">
+  <div class="row align-items-center mb-5 mt-5 pt-5">
+    <div class="col-12">
+      <img src="@/assets/pokemon_logo.svg" alt="Logo" class="d-inline-block align-text-top">
+    </div>
+  </div>
+  <div class="row align-items-center">
+      <div class="col-12">
+          <RouterLink class="btn btn-dark fs-4" @click="back" to="/pokemons">Pokemons List</RouterLink>
+      </div>
+  </div>        
 </div>
 </template>
 <style scoped>
-    #building{
-        background:url("../assets/criaturas-pokemon.jpg");    
-        width:100vw;			
-        height:100%;			
-        position:fixed;
-        background-size:100% 100%;
-        
+
+    img{
+      width: 50%;
+      animation: puff-in-center 0.7s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
     }
 
-    p{
-        animation: puff-in-center 0.7s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+    .btn{
+      animation: bounce-in-left 1.1s 1.1s both;
     }
 
     @keyframes puff-in-center {
-  0% {
-    transform: scale(2);
-    filter: blur(4px);
-    opacity: 0;
+      0% {
+        transform: scale(2);
+        filter: blur(4px);
+        opacity: 0;
+      }
+      100% {
+        transform: scale(1);
+        filter: blur(0px);
+        opacity: 1;
+      }
+    }
+    @keyframes bounce-in-left {
+      0% {
+        transform: translateX(-600px);
+        animation-timing-function: ease-in;
+        opacity: 0;
+      }
+      38% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+        opacity: 1;
+      }
+      55% {
+        transform: translateX(-68px);
+        animation-timing-function: ease-in;
+      }
+      72% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      81% {
+        transform: translateX(-28px);
+        animation-timing-function: ease-in;
+      }
+      90% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      95% {
+        transform: translateX(-8px);
+        animation-timing-function: ease-in;
+      }
+      100% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+    }
+
+/*Media query*/
+
+/* @media (max-width: 768px) {
+  button{
+    width: 200px;
   }
-  100% {
-    transform: scale(1);
-    filter: blur(0px);
-    opacity: 1;
-  }
-}
+} */
 </style>
 
